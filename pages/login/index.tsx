f0a6f36025eeb836150fbd4c388457d6d312a11f
brain <vaibhav.satam@accionlabs.com>
2022-09-07 22:21:46 +0530

    modified login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,19 +1,14 @@
 const Login: React.FC<Props> = () => {
     return (
-        <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 h-screen">
-            <div className="w-full max-w-md space-y-8 bg-gray-50">
+        <div className="h-screen flex flex-1 py-12 px-4 items-center justify-center">
+            <div className="w-full max-w-md">
                 <div>
-                    <img
-                        className="mx-auto h-12"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="IDP"
-                    />
                     <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
                 </div>
-                <form className="mt-8 space-y-6" action="#" method="POST">
-                    <div className="-space-y-px rounded-md shadow-sm">
+                <form className="mt-8 space-y-6">
+                    <div className="rounded-md shadow-sm">
                         <div>
                             <label htmlFor="email-address" className="sr-only">
                                 Email address
@@ -24,7 +19,7 @@ const Login: React.FC<Props> = () => {
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                className="relative block w-full  border border-gray-300 px-3 py-2 text-gray-900"
                                 placeholder="Email address"
                             />
                         </div>
@@ -38,17 +33,14 @@ const Login: React.FC<Props> = () => {
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                className="relative block w-full  border border-gray-300 px-3 py-2 text-gray-900"
                                 placeholder="Password"
                             />
                         </div>
                     </div>
-                    <button
-                        type="submit"
-                        className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        Sign in
-                    </button>
+                    <div className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
             </div>
         </div>
