b6c9196d14de10f3fe2c1d6da441874ac4d3462e
brain <vaibhav.satam@accionlabs.com>
2022-09-07 22:25:01 +0530

    modified home page

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,13 +6,11 @@ const Header: React.FC<Props> = (props) => {
 
     return (
         <div className="shadow-xl">
-            <div className="flex flex-1 items-center bg-gray-800 justify-between px-4">
+            <div className="flex flex-1 items-center bg-indigo-600 justify-between px-4">
                 <div className="flex flex-shrink-0 items-center h-16 ">
-                    <img
-                        className="mx-auto h-12 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt="IDP"
-                    />
+                    <h2 className="text-white font-medium text-lg">
+                        Identify Management
+                    </h2>
                 </div>
                 <Button title="Login" onClick={() => router.push("/login")} />
             </div>
