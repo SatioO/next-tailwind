dc64f75d32f9abacfa2af3073dec36c93c1c9c2b
brain <vaibhav.satam@accionlabs.com>
2022-09-08 16:14:41 +0530

    fixed minor issues

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,19 +1,29 @@
-const Button: React.FC<Props> = ({ title, color = "white", onClick }) => {
-    console.log("color", color)
-    return (
-        <button
-            onClick={onClick}
-            className={"px-8 py-2 bg-" + color + " rounded-sm shadow-2xl"}
-        >
-            <p className="text-black font-medium">{title}</p>
-        </button>
-    )
-}
+import { forwardRef, PropsWithChildren } from "react"
+
+const Button = forwardRef<HTMLButtonElement, PropsWithChildren<Props>>(
+    (props, ref) => {
+        return (
+            <button
+                ref={ref}
+                onClick={props.onClick}
+                className={
+                    "px-8 py-2 bg-" +
+                    (props.color || "white") +
+                    " rounded-sm shadow-2xl bg-"
+                }
+            >
+                <p className="text-black font-medium">{props.title}</p>
+            </button>
+        )
+    }
+)
 
 type Props = {
     title: string
     color?: string
     onClick?: () => void
 }
+
+Button.displayName = "FancyButton"
 
 export default Button
